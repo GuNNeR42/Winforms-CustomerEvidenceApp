cc495b7cba98ba50cc0215eca9377234c63b48de
Max Rataj <maxmilianrataj@gmail.com>
2024-11-02 21:33:53 +0100

    Add CustomerList Form

---
+++ Forms/CustomerListForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace CustomerEvidenceApp.Forms
+{
+    partial class CustomerListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewCustomers = new DataGridView();
+            button_addCustomer = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCustomers).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewCustomers
+            // 
+            dataGridViewCustomers.AllowUserToOrderColumns = true;
+            dataGridViewCustomers.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewCustomers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCustomers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCustomers.Location = new Point(12, 12);
+            dataGridViewCustomers.Name = "dataGridViewCustomers";
+            dataGridViewCustomers.RowHeadersWidth = 46;
+            dataGridViewCustomers.Size = new Size(776, 392);
+            dataGridViewCustomers.TabIndex = 0;
+            // 
+            // button_addCustomer
+            // 
+            button_addCustomer.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_addCustomer.Location = new Point(12, 412);
+            button_addCustomer.Name = "button_addCustomer";
+            button_addCustomer.Size = new Size(125, 26);
+            button_addCustomer.TabIndex = 1;
+            button_addCustomer.Text = "Add Customer";
+            button_addCustomer.UseVisualStyleBackColor = true;
+            button_addCustomer.Click += button_addCustomer_Click;
+            // 
+            // CustomerListForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button_addCustomer);
+            Controls.Add(dataGridViewCustomers);
+            Name = "CustomerListForm";
+            Text = "CustomerListForm";
+            Load += CustomerListForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCustomers).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewCustomers;
+        private Button button_addCustomer;
+    }
+}
---
+++ Forms/CustomerListForm.cs
@@ -0,0 +1,42 @@
+﻿using CustomerEvidenceApp.Data;
+using CustomerEvidenceApp.Data.Models;
+using System.ComponentModel;
+
+namespace CustomerEvidenceApp.Forms
+{
+    public partial class CustomerListForm : Form
+    {
+        private readonly SQLiteDbContext _context;
+        private BindingList<Customer> _customers;
+
+        public CustomerListForm(SQLiteDbContext context)
+        {
+            InitializeComponent();
+            _context = context;
+
+            _customers = new BindingList<Customer>(_context.Customers.ToList());
+        }
+
+        private void CustomerListForm_Load(object sender, EventArgs e)
+        {
+            // Load Customers from databse and convert it to binding list
+            
+            dataGridViewCustomers.DataSource = _customers;
+        }
+
+        private void button_addCustomer_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+
+            AddCustomerForm addCustomerForm = new AddCustomerForm(customer);
+
+            if (DialogResult.OK == addCustomerForm.ShowDialog())
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+
+                _customers.Add(customer);
+            }
+        }
+    }
+}
