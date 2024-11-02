ff88435b797af344e569960df7482b0ff8deb4ae
Max Rataj <maxmilianrataj@gmail.com>
2024-11-02 17:55:19 +0100

    Make Customers migration

---
+++ Data/Migrations/20241102165045_CreateCustomersTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerEvidenceApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCustomersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    full_name = table.Column<string>(type: "TEXT", nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    iq = table.Column<int>(type: "INTEGER", nullable: false),
+                    gender = table.Column<string>(type: "TEXT", nullable: false),
+                    gdpr_agreement = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
